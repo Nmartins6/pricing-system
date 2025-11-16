cd96d7d95eb7697ee4ed7dd3fdb3c755f8171e3d
Nicolas Martins <nicolas.edm7@gmail.com>
2025-11-16 14:04:10 -0300

    Initial commit

---
+++ src/test/java/dev/pricing_system/PricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pricing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
